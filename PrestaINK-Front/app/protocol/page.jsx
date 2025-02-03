0640da6e179bb279a44360036ff7681641631d84
Alejandro <95312462+AGMASO@users.noreply.github.com>
2025-02-03 17:56:50 +0100

    Add Alert when minting Prink

--- PrestaINK-Front/app/protocol/page.jsx
+++ PrestaINK-Front/app/protocol/page.jsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useAccount } from "wagmi";
+import checkBalanceCollateral from "../lib/scripts/checkBalanceCollateral";
+import { ethers } from "ethers"; // Importing ethers
+import { useState, useEffect } from "react";
 
 import MenuMain from "../ui/menu-main";
 import CodersComp from "../ui/coderscomp";
@@ -10,6 +13,36 @@ import ProtocolActions from "../ui/protocolComp-Actions";
 
 export default function Protocol() {
   const { address, isConnected, isDisconnected } = useAccount();
+  const [wETH, setwETH] = useState();
+  const [balancePRINK, setBalancePRINK] = useState();
+  const [usdValueCollateral, setUsdValueCollateral] = useState();
+
+  useEffect(() => {
+    async function checkBalanceCollateralEveryChange() {
+      try {
+        const { balanceWEth, balancePRINK, usdValueCollateral } =
+          await checkBalanceCollateral(address);
+
+        console.log("Esto es usdValue", usdValueCollateral);
+        setwETH(ethers.utils.formatUnits(balanceWEth, "ether"));
+        console.log(
+          "Esto es TTTTTT",
+          ethers.utils.formatUnits(balancePRINK, "ether")
+        );
+        setBalancePRINK(ethers.utils.formatUnits(balancePRINK, "ether"));
+        setUsdValueCollateral(
+          ethers.utils.formatUnits(usdValueCollateral, "ether")
+        );
+      } catch (error) {
+        console.error("Failed to fetch balances: ", error);
+      }
+    }
+
+    if (address) {
+      checkBalanceCollateralEveryChange();
+    }
+  }, [address]); // Dependency on address to re-run when it changes
+
   return (
     <div className='min-h-screen'>
       {isDisconnected && <MenuNotConnected></MenuNotConnected>}
@@ -30,11 +63,20 @@ export default function Protocol() {
                   <p className=' text-white text-center mb-10 mt-10'>
                     Your Stats in the Protocol
                   </p>
-                  <ProtocolCheckBalance addressUser={address} />
+                  <ProtocolCheckBalance
+                    addressUser={address}
+                    wETHCollateral={wETH}
+                    prinkMinted={balancePRINK}
+                    usdValueCollateral={usdValueCollateral}
+                  />
                 </div>
                 <div className='flex flex-col justify-center items-center  w-[50%]'>
                   <p className=' text-white text-center mb-10 mt-10'></p>
-                  <ProtocolActions addressUser={address}></ProtocolActions>
+                  <ProtocolActions
+                    addressUser={address}
+                    prinkMinted={balancePRINK}
+                    usdValueCollateral={usdValueCollateral}
+                  ></ProtocolActions>
                 </div>
               </div>
             </>
